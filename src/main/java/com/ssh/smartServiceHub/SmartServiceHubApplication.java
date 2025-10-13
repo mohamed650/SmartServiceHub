b97c7ce765b5939ff6d2c2eefdf4efd207e82062
mohamed650 <kaleemkhalid331@gmail.com>
2025-10-13 14:08:56 +0530

    first commit

---
+++ src/main/java/com/ssh/smartServiceHub/SmartServiceHubApplication.java
@@ -0,0 +1,13 @@
+package com.ssh.smartServiceHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartServiceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartServiceHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssh/smartServiceHub/SmartServiceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssh.smartServiceHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartServiceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
